d67a3867bccfc796718f682705f136d86005f10a
ashley.fernandes <ashley.fernandes@upstox.com>
2020-03-10 17:46:02 +0530

    add build folder

---
+++ build/precache-manifest.6e43a52726039e6d5743416e2d6664a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6212665b3ad9248403e860a8a4c47ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d93ddbd6b6894c81996",
+    "url": "/static/css/main.ee1905e2.chunk.css"
+  },
+  {
+    "revision": "eaa2b1af8e5726303e61",
+    "url": "/static/js/2.309174e9.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/static/js/2.309174e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d93ddbd6b6894c81996",
+    "url": "/static/js/main.efeeb77d.chunk.js"
+  },
+  {
+    "revision": "3c65d78fa21bcf0faac8",
+    "url": "/static/js/runtime-main.453dfe1e.js"
+  }
+]);
